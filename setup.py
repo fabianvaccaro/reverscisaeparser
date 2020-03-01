369d327f4f57d343e2403704104a1493f7c9c8ea
Fabián <fabianvaccaro@hotmail.com>
2020-03-01 04:10:28 +0100

    first

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name="surveylabparser",
+      version="0.1.1",
+      description="Reverse Parsing for the CISA SurveyLab XML",
+      url="http://cmgi.uma.es",
+      author="CM&GI",
+      author_email="info@cmgi.uma.es",
+      license="MIT",
+      packages=["surveylabparser"],
+      install_requires=["numpy"],
+      zip_safe=False)
